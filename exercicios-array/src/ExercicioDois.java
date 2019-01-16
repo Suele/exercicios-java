494249d98a516ab03818ba5d2f451d9d42ad09ee
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-01-16 13:38:34 -0200

    Adiciona o exercicio numero 2 que insere 10 elementos em ordem crecente.

---
+++ exercicios-array/src/ExercicioDois.java
@@ -0,0 +1,15 @@
+/*
+ * O programa insere 10 elementos em um array de 10 ate 0.  
+ */
+
+public class ExercicioDois {
+	public static void main(String[] args) {
+
+		int[] crecente = new int[11];
+
+		for (int i = 10; i >= 0; i--) {
+			System.out.println(crecente[i] = i);
+		}
+
+	}
+}
