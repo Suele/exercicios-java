29a42f313da402552799a9a3d608b373a0395b6f
Suele <suelen.ads.guimaraes@gmail.com>
2019-01-16 23:34:19 -0200

    Adiciona o exercicio tres.

---
+++ exercicios-array/src/ExercicioTres.java
@@ -0,0 +1,26 @@
+
+public class ExercicioTres {
+	public static void main(String[] args) {
+
+		// 1, 0, 5, -2, -5, 7
+		int[] A = new int[6];
+
+		A[0] = 1;
+		A[1] = 0;
+		A[2] = 5;
+		A[3] = -2;
+		A[4] = -5;
+		A[5] = 7;
+
+		int soma = A[0] + A[1] + A[5];
+
+		System.out.println(soma);
+
+		A[4] = 100;
+
+		for (int i = 0; i < A.length; i++) {
+			System.out.println(A[i]);
+		}
+
+	}
+}
