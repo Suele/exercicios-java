c0150615ca1c560ecda6f305ba71bc7c12cd59fa
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-01-24 15:15:55 -0200

    Adiciona o exercicio TesteInterfaceCollection que mostra a diferenção entre a interface List e a Collection neste caso quase que nenhum.

---
+++ exercicios-array/src/exercicio/com/collection/TesteInterfaceCollection.java
@@ -0,0 +1,32 @@
+package exercicio.com.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import exercicio.array.tipo.objeto.Cliente;
+
+public class TesteInterfaceCollection {
+
+	public static void main(String[] args) {
+
+		Cliente c1 = new Cliente("Maria da Silva", "Rua das Flores");
+		Cliente c2 = new Cliente("Paula da Silveira", "Rua das Palmeiras");
+		Cliente c3 = new Cliente("Amanda Pereira", "Rua dos Cravos");
+
+		Collection<Cliente> clientes = new ArrayList<Cliente>();
+		clientes.add(c1);
+		clientes.add(c2);
+		clientes.add(c3);
+
+		System.out.println("Clientes cadastradas.");
+		for (Cliente cliente : clientes) {
+			System.out.println(cliente.getNome());
+		}
+		
+		//System.out.println("Busca pelo indice: " + clientes.get(1));
+		
+		System.out.println(clientes + "\n");
+	}
+
+}
