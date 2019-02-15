8522bc57cfe292b79bce53d98448039b10fe7759
Suelen Guimarães <suelen.ads.guimaraes@gmail.com>
2019-02-15 21:58:24 -0200

    Adiciona um for para adicionar os elementos, outro for para verificar qual é o maior elemento dentro do array e outro for para contar quantos elementos iguais ao maior valor aparecem no array.

--- exercicios-array/src/BoloDeAniversario.java
+++ exercicios-array/src/BoloDeAniversario.java
@@ -1,3 +1,4 @@
+
 /*
  * Conta quantos elementos dentro do array são iguais.*/
 
@@ -10,51 +11,50 @@ public class BoloDeAniversario {
 		Scanner entradaDeDados = new Scanner(System.in);
 
 		int tamanhoDoArray = entradaDeDados.nextInt();
-		int vela = 0;
+		String vela = null;
 		int maiorVela = 1;
 		int maiorValor = 0;
-		int contaVelasMaiores = 0;
+		int contandoMaiorValor = 0;
 
 		int[] bolo = new int[tamanhoDoArray];
 
 		// para inserir dados.
 		for (int i = 0; i < bolo.length; i++) {
-			vela = entradaDeDados.nextInt();
-			bolo[i] = vela;
+			vela = entradaDeDados.next();
+			bolo[i] = Integer.parseInt(vela);
+
 		}
 
 		for (int j = 0; j < bolo.length && maiorVela < bolo.length; j++) {
-
+			
+			// se for a primeira interação verifica as duas posições qual é a maior.
 			if (j == 0) {
-				if (bolo[maiorVela] > bolo[j]) {
-					maiorValor = bolo[maiorVela];
-					contaVelasMaiores++;
-				} else if (bolo[j] > bolo[maiorVela]) {
+				if (bolo[j] > bolo[maiorVela]) {
 					maiorValor = bolo[j];
-					contaVelasMaiores++;
-				} else if (bolo[j] == bolo[maiorVela]) {
-					contaVelasMaiores++;
+				} else if (bolo[maiorVela] > bolo[j]) {
+					maiorValor = bolo[maiorVela];
 				}
 			}
 
-			if (j > 0 && j < bolo.length) {
-				if (bolo[maiorVela] > bolo[j] && bolo[maiorVela] > maiorValor) {
-					maiorValor = bolo[maiorVela];
-				} else if (bolo[j] > bolo[maiorVela] && bolo[j] > maiorValor) {
+			// se o maiorValor for menor que algum elemento do array.
+			// então altera-se pelo valor do array.
+			if (j > 0) {
+				if (bolo[j] > maiorValor) {
 					maiorValor = bolo[j];
-				} else if (bolo[j] == bolo[maiorVela] && bolo[j] == maiorValor) {
-					maiorValor = bolo[j];
-					contaVelasMaiores++;
-				} else if (bolo[maiorVela] > bolo[j] && bolo[maiorVela] == maiorValor) {
-					maiorValor = bolo[maiorVela];
-					contaVelasMaiores++;
-				} else if (bolo[j] > bolo[maiorVela] && bolo[j] == maiorValor) {
-					maiorValor = bolo[j];
-					contaVelasMaiores++;
 				}
+
 			}
 
 		}
-		System.out.println(contaVelasMaiores);
+
+		// conta quantos elementos iguais ao maior valor tem no array.
+		for (int v = 0; v < bolo.length; v++) {
+			if (bolo[v] == maiorValor) {
+				contandoMaiorValor++;
+			}
+		}
+
+		System.out.println(maiorValor);
+		System.out.println(contandoMaiorValor);
 	}
 }
